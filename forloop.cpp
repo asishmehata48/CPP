5fd1de1fffd8ef066786aa24dd4c5dc53595dd91
Asish Mehata <166911195+asishmehata48@users.noreply.github.com>
2024-08-21 17:24:45 +0000

    C++

--- forloop.cpp
+++ forloop.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-
+using namespace std;
 int main() {
     for (int i = 1; i <= 5; i++) {
-        std::cout << "Count: " << i << std::endl;
+        cout << "Count: " << i << std::endl;
     }
     return 0;
 }

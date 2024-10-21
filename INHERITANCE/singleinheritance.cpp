16fe327577c35bfc9c0e65c0e35e6a98a0827de1
jelina bhatt <jelinabhatt1@gmail.com>
2024-10-21 20:03:48 +0545

    simgle inheritance code modified

--- INHERITANCE/singleinheritance.cpp
+++ INHERITANCE/singleinheritance.cpp
@@ -1,40 +1,41 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class A
-{
-    int x , y;
+class A {
+    int x, y;
 
-    void show()
-    {
-        cout<<x<<y;
+public:
+    int z;
+
+    void setValues(int a, int b) {
+        x = a;
+        y = b;
     }
-    public: int z;
-    void sum()
-    {
-        z = x+y;
-        cout<<z;
+
+    void sum() {
+        z = x + y;
+        cout << z << endl;
     }
 };
 
-class B : public A
-{
-    int a , b;
-    public: void enter()
-    {
-        cin>> a >> b;
+class B : public A {
+    int a, b;
+
+public:
+    void enter() {
+        cout<<"enter the values of a and b";
+        cin >> a >> b;
+        setValues(a, b); // Set x and y using values from B
     }
-    void display()
-    {
-        cout<< a <<b;
+
+    void display() {
+        cout << a << " " << b << endl;
     }
 };
 
-int main()
-{
-    
+int main() {
     B n;
-    n.z = 20;
-    n.sum();
+    n.enter();  // Input for a and b, which sets x and y
+    n.sum();    // Calculate and display the sum
     return 0;
 }

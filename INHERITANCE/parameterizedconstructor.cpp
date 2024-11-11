47aac107b9b6912c46531159e7a14c902d84dc1b
Asish Mehata <mehataasish@gmail.com>
2024-11-11 21:05:52 +0545

    Parameterized Constructor in single inheritance base

---
+++ INHERITANCE/parameterizedconstructor.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+class base{
+    protected: int mark1, mark2;
+    public: base(int a, int b)
+    {
+        mark1 = a;
+        mark2 = b;
+    }
+};
+
+class derived: public base{
+    int total;
+    public: derived() : base (0,0)
+    {
+        total = 0;
+    }
+    int calculate()
+    {
+        cout<<"total work ="<<mark1+mark2;
+    }
+};
+
+int main()
+{
+    derived dl;
+    dl.calculate();
+    return 0;
+}

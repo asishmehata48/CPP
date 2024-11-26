dd70ea28fb40e3d5bb7a054a5ef4225323b56b7f
Asish Mehata <mehataasish@gmail.com>
2024-11-26 22:19:49 +0545

    function overloaded in templates

--- Template/add_twono.cpp
+++ Template/add_twono.cpp
@@ -16,11 +16,10 @@ int sum (int a , int b)
     return (a+b);
 }
 
-int main()
-{
-    int a=2, b=3;
-    float c= 4.4;
-    cout<<"Sum of a & b "<<sum(a,b);
-    cout<<"Sum of b & c "<<sum(b,c);
+int main(){
+    int a=5, b=7;
+    float c= 3.2, d = 5.7;
+    cout<<"Sum of a and b "<<sum(a,b)<<endl;
+    cout<<"Sum of c and d "<<sum(c,d);
     return 0;
 }
